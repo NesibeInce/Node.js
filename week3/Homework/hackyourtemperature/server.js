72f83c738657fa3ddffb6d38954daff394bf4e88
NesibeInce <nesybe.ince@gmail.com>
2019-12-09 00:57:07 +0100

    hackyourtemperature 3

--- week3/Homework/hackyourtemperature/server.js
+++ week3/Homework/hackyourtemperature/server.js
@@ -1,23 +1,54 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
 const app = express();
+const axios = require('axios');
+const API_KEY = require('./sources/keys.json').API_KEY;
 
 app.set('view engine', 'handlebars');
-app.engine('handlebars', exphbs({
-  defaultLayout: 'main'
-}));
+app.engine(
+  'handlebars',
+  exphbs({
+    defaultLayout: 'main',
+  }),
+);
 
 app.use(express.json());
-app.use(express.urlencoded({
-  extended: true
-}));
+app.use(
+  express.urlencoded({
+    extended: true,
+  }),
+);
 
 app.get('/', (req, res) => {
   res.render('index');
 });
 
-app.post('/weather', (req, res) => {
-  res.send(req.body.cityName);
+app.post('/weather', async (req, res) => {
+  let cityName = req.body.cityName;
+  const url = `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}`
+
+  try {
+    const data = await axios(url);
+    console.log(data.data.cod);
+    const cityTemp = data.data.main.temp;
+    let weatherText = `The temperature for the ${cityName} is ${cityTemp} °F today`;
+    res.render('index', {
+      weatherText
+    })
+  } catch (error) {
+    const err = error.response.data;
+    let cityNotFound = `${cityName} is not found!`
+    if (err.cod == 404) {
+      res.render('index', {
+        cityNotFound
+      })
+    } else {
+      let errMsg = err.message
+      res.render('index', {
+        errMsg
+      })
+    }
+  };
 });
 
 app.listen(3000, () => {
